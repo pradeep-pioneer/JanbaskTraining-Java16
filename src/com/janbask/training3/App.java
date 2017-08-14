bfb0b229f7b6d5d94c6c20c95dbc9f14d6b2f36f
Pradeep Singh <pradeep.ansh.sumit@gmail.com>
2017-08-14 15:01:40 +0530

    Added the vcs.xml Added the logic for Introspector

--- src/com/janbask/training3/App.java
+++ src/com/janbask/training3/App.java
@@ -4,16 +4,36 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.*;
 
 public class App {
     private JButton buttonMessage;
     private JPanel containerPanel;
+    private JButton buttonBeanInfo;
 
     public App() {
         buttonMessage.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JOptionPane.showMessageDialog(null, "You clicked on the first button");
+            }
+        });
+        buttonBeanInfo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    BeanInfo beanInfo = Introspector.getBeanInfo(JFrame.class);
+                    BeanDescriptor beanDescriptor = beanInfo.getBeanDescriptor();
+                    String beanData = String.format("Display Name: %s\nShort Description: %s",
+                            beanDescriptor.getDisplayName(),
+                            beanDescriptor.getShortDescription());
+                    JOptionPane.showMessageDialog(null, beanData);
+                }
+                catch (IntrospectionException introspectionException){
+                    String messageData = String.format("There was an error!\nMessage: %s",
+                            introspectionException.getMessage());
+                    JOptionPane.showMessageDialog(null, messageData);
+                }
             }
         });
     }
